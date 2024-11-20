485151c0b45d4252aee7181d25e26f7e092579c2
Sylwester Slusarczyk <Sylwek@wlasciciel.com>
2024-11-20 21:48:03 +0100

    put and post update

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,71 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 10000;
+
+/*
+{
+  nick : "ala",
+  clientId : "1234-5678"
+}
+
+*/
+const ClientDatabase = [];
+const ClientMess = [];
+
+//////////////// modul /////
+
+function generateUuid() {
+  // sprawdza czy sie nie powtorzy, sprawdz czy nie istnieje wbudowana w Node metoda
+  return 1111; // unique value
+}
+
+///////////////////////
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
+app.post('/signin',(req, res) => { // zmieniam stan serwera 
+  // co dostaje? -> dostajemy Nick klienta
+  // co zwracam? -> generujemy unikalny CLIENT ID, zapis do bazy danych klientow, zwracamy status powowdzenia akcji oraz CLIENT ID
+  
+  const entry = {
+    nick : req.query["nick"],
+    clientId : generateUuid()
+  }
+
+  ClientDatabase.push(entry);
+  res.status(201).send(entry);
+})
+
+app.put('/msg',(req, res) => {  // rejest danych na serwerze 
+  // co dostaje? -> dostajemy dane od klienta do zapisu w bazie wiadomosci
+  // co zwracam? -> zwracam status http, czy pomyslnie sie udalo (200 to all OK)
+  const messageDatabaseItem = {
+    clientId : req.body.clientId,
+    contentType : req.body.contentType,
+    content : req.body.message,
+    destinationClientId : req.body.destinationClientId,
+    synchroStatus : false,
+    date : Date.now()
+  }
+  ClientMess.push(messageDatabaseItem)
+  res.status(201);
+  res.send();
+})
+
+app.get('/msg',(req, res) => { // otrzymujemy
+  // co dostaje? -> Client ID 
+  // co zwracam? -> lista damych dla niego oraz zmieniam status wiadomosci na dostarczone do klienta  
+  res.status(310).send(`Get message: ${ req.query["client_id"]}`); 
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
